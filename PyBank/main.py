38c21c64db7767988ce370326ed0c68692ed490e
Wayne Odd <w_odd@u.pacific.edu>
2021-07-12 13:38:55 -0700

    99 percent complete, need terminal output

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,6 +6,7 @@ values=[]
 changes=[]
 
 
+
 csvpath = os.path.join('.', 'Resources','budget_data.csv')
 
 with open(csvpath) as budgetfile:
@@ -18,11 +19,12 @@ with open(csvpath) as budgetfile:
     for data in csvreader:
         values.append(int(data[1]))
         months.append(data[0])     
-    totalMonths = len(months)
+    totalMonths = int(len(months))
     
     
     #Total amount P/L over entire period
-    totalAmt = "{:,}".format(sum(values))
+    totalAmt = sum(values)
+    ftotalAmt = "{:,}".format(sum(values))
 #     print(totalAmt)  
 
 
@@ -32,12 +34,16 @@ with open(csvpath) as budgetfile:
         changes.append(change) 
     
     #Calculate avg of changes
-    avgChange = "{:,}".format(round(sum(changes)/len(changes),2))
+    avgChange = round(sum(changes)/len(changes),2)
+    favgChange = "{:,}".format(round(sum(changes)/len(changes),2))
 #     print(round(avgChange,2))
     
     
-    maxChange = "{:,}".format(max(changes))
-    minChange = "{:,}".format(min(changes))
+    maxChange = max(changes)
+    minChange = min(changes)
+    fmaxChange = "{:,}".format(max(changes))
+    fminChange = "{:,}".format(min(changes))
+    
     
 #     print(maxChange)
 #     print(minChange)
@@ -46,36 +52,42 @@ with open(csvpath) as budgetfile:
     for x in range(len(changes)-1):
         if maxChange == changes[x]:
             maxDateIdx = int(changes.index(changes[x]))   
-        
+            
         if minChange == changes[x]:
             minDateIdx = int(changes.index(changes[x]))
-    
+            break
  
     # print(maxDateIdx)
 #     print(minDateIdx)
 
     #Use value index to match date in months list
-    profitInc = months[maxDateIdx + 1]
-    profitDec = months[minDateIdx + 1]
+    profitInc = months[maxDateIdx+1]
+    profitDec = months[minDateIdx+1]
 
     #Print Analysis
     print(f"Finacial Analysis")
     print('-' * 25)
     print(f"Total Months: {totalMonths}")
-    print(f"Total: ${totalAmt}")
-    print(f"Average Change: ${avgChange}")
-    print(f"Greatest Increase in Profits: {profitInc} (${maxChange})")
-    print(f"Greatest Decrease in Profits: {profitDec} (${minChange})")
+    print(f"Total: ${ftotalAmt}")
+    print(f"Average Change: ${favgChange}")
+    print(f"Greatest Increase in Profits: {profitInc} (${fmaxChange})")
+    print(f"Greatest Decrease in Profits: {profitDec} (${fminChange})")
     
-    #     #Export text file of results
-#     cleanCsv = zip([totalAmt])
-    cleanCsv = zip([totalAmt, avgChange, profitInc, maxChange, profitDec, minChange])
-    
-    
-    output_path = os.path.join(".", "Analysis", "PyBank_Results")
+     #Export text file of results
+    cleanCsv = zip([[totalMonths , totalAmt , avgChange , profitInc , maxChange , profitDec , minChange]])
+
+    output_path = os.path.join(".", "Analysis", "PyBank_Results.txt")
     
     with open(output_path, 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=',')
+        csvwriter = csv.writer(csvfile, delimiter=' ')
+        csvwriter2 = csv.writer(csvfile, delimiter=',')
         csvwriter.writerow(['Finacial Analysis'])
-        csvwriter.writerows(['Total Months','Total Profit','Average Change','Increase Month','Greatest Increase in Profits','Decrease Month','Greatest Decrease in Profits'])
-        csvwriter.writerow(cleanCsv)
+        csvwriter.writerow(['--------------------'])
+        csvwriter.writerow(['Total Months: ' , totalMonths])
+        csvwriter.writerow(['Total: $' , ftotalAmt])
+        csvwriter.writerow(['Average Change: $', favgChange])
+        csvwriter.writerow(['Greatest Increase in Profits: $', fmaxChange, profitInc])
+        csvwriter.writerow(['Greatest Decrease in Profits: $', fminChange, profitDec])
+        csvwriter.writerow(['--------------------'])
+        csvwriter2.writerows([['Total Months','Total Profit','Average Change','Increase Month','Greatest Increase in Profits','Decrease Month','Greatest Decrease in Profits']])
+        csvwriter2.writerow(cleanCsv)
